08a591749c38eb76a7a8b617a89c89e1288161c6
AntonioLuisGlez <algonzalez.ext@catec.aero>
2024-06-25 11:03:33 +0200

    RTP_qualcomm now is configurable with gstreamer pipeline parameters

--- src/remote_cmd_executor/src/execute_remote_cmd.py
+++ src/remote_cmd_executor/src/execute_remote_cmd.py
@@ -15,13 +15,15 @@ def execute_command(command):
         rospy.logerr("Failed to execute command: %s", e.stderr.decode())
 
 
-def remote_cmd_executor():
+def remote_cmd_executor(IR_width, IR_height, IR_framerate, IR_IP_address, IR_UDP_port,
+                        IR_encoder_tune, IR_encoder_bitrate, IR_encoder_speed_preset,
+                        EO_width, EO_height, EO_framerate, EO_IP_address, EO_UDP_port):
     rospy.init_node('remote_cmd_executor', anonymous=True)
 
     # Define your command here
-    command = "gst-launch-1.0 -e \
-v4l2src device=/dev/video0 ! video/x-raw,format=I420,width=640,height=514,framerate=60/1 ! queue ! x264enc tune=3 bitrate=10000 speed-preset=ultrafast ! h264parse config-interval=-1 ! rtph264pay ! udpsink host=10.42.0.1 port=5000 sync=false \
-qtiqmmfsrc name=qmmf ! video/x-h265,format=NV12,width=1920,height=1080,framerate=30/1 ! h265parse config-interval=-1 ! rtph265pay ! udpsink host=10.42.0.1 port=5001 sync=false"
+    command = f"gst-launch-1.0 -e \
+v4l2src device=/dev/video0 ! video/x-raw,format=I420,width={IR_width},height={IR_height},framerate={IR_framerate} ! queue ! x264enc tune={IR_encoder_tune} bitrate={IR_encoder_bitrate} speed-preset={IR_encoder_speed_preset} ! h264parse config-interval=-1 ! rtph264pay ! udpsink host={IR_IP_address} port={IR_UDP_port} sync=false \
+qtiqmmfsrc name=qmmf ! video/x-h265,format=NV12,width={EO_width},height={EO_height},framerate={EO_framerate} ! h265parse config-interval=-1 ! rtph265pay ! udpsink host={EO_IP_address} port={EO_UDP_port} sync=false"
 
     rospy.loginfo("Executing command directly: %s", command)
     ssh_command = f"ssh username@qualcomm_ip '{command}'"
@@ -30,6 +32,26 @@ qtiqmmfsrc name=qmmf ! video/x-h265,format=NV12,width=1920,height=1080,framerate
 
 if __name__ == '__main__':
     try:
-        remote_cmd_executor()
+        # Fetching parameters from ROS launch
+        IR_width = rospy.get_param('/IR_width', '640')
+        IR_height = rospy.get_param('/IR_height', '514')
+        IR_framerate = rospy.get_param('/IR_framerate', '60/1')
+        IR_IP_address = rospy.get_param('/IR_IP_address', '10.42.0.1')
+        IR_UDP_port = rospy.get_param('/RTP_PORT_cam1', '5000')
+        IR_encoder_tune = rospy.get_param('/IR_encoder_tune', '3')
+        IR_encoder_bitrate = rospy.get_param('/IR_encoder_bitrate', '10000')
+        IR_encoder_speed_preset = rospy.get_param(
+            '/IR_encoder_speed-preset', 'ultrafast')
+
+        EO_width = rospy.get_param('/EO_width', '1920')
+        EO_height = rospy.get_param('/EO_height', '1080')
+        EO_framerate = rospy.get_param('/EO_framerate', '30/1')
+        EO_IP_address = rospy.get_param('/EO_IP_address', '10.42.0.1')
+        EO_UDP_port = rospy.get_param('/RTP_PORT_cam2', '5001')
+
+        remote_cmd_executor(IR_width, IR_height, IR_framerate, IR_IP_address, IR_UDP_port,
+                            IR_encoder_tune, IR_encoder_bitrate, IR_encoder_speed_preset,
+                            EO_width, EO_height, EO_framerate, EO_IP_address, EO_UDP_port)
+
     except rospy.ROSInterruptException:
         pass
